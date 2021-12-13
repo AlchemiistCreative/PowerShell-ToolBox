ac8bd307b9e1a8a379144c8e1036e2c99361d107
Thomas François <68130614+AlchemiistCreative@users.noreply.github.com>
2021-12-13 14:23:01 +0100

    Update add-accessrights.ps1

--- add-accessrights.ps1
+++ add-accessrights.ps1
@@ -1,22 +1,18 @@
 param(
-[string]$user
+[string]$user,
+[string]$group
 )
 
-$users = Get-ADGroupMember ShowroomUsers 
+$users = Get-ADGroupMember $group 
 
 ForEach ($user_ in $users) {
 
-
 if ($user_.SamAccountName -ne $user){
-
-        
-       
     Add-MailboxPermission `
         -Identity $user_.name `
         -User $user `
         -AccessRights FullAccess `
         -InheritanceType All
-        
 }
    
 
